a1a036aefae777e2d9f37880ae504110aac171f0
hvok01 <lecofdesings2015@gmail.com>
2023-09-06 15:41:11 -0300

    mobile mockup advanced

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import TwitterIcon from './assets/twitterIcon';
 import InstagramIcon from './assets/instagramIcon';
 import YoutubeIcon from './assets/youtubeIcon';
 import carmine from "./assets/carmine_01.jpg";
+import carmine2 from "./assets/carmine_02.jpg";
+import carmine3 from "./assets/carmine_03.jpeg";
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -27,14 +29,7 @@ function App() {
   useLayoutEffect(() => {
 
     const ctx = gsap.context(() => {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: showImagePartOne.current,
-          // markers: true,
-          start: "-50% center",
-          end: "200% center"
-        }
-      });
+      const tl = gsap.timeline();
 
       tl.from(crossOne.current, {
         opacity: 0,
@@ -67,6 +62,7 @@ function App() {
 
     const lineOne = document.getElementById("header-line-1");
     const lineTwo = document.getElementById("header-line-2");
+    const header = document.getElementById("header-main");
 
     if(!timeline?.isActive()) {
 
@@ -97,9 +93,10 @@ function App() {
               opacity: 1,
               display: "flex",
               delay: -0.5,
+              y: "+10",
             }).fromTo(navLinksCurrent, {
               opacity: 0,
-              display: "none"
+              display: "none",
             },{
               opacity: 1,
               display: "flex",
@@ -123,6 +120,10 @@ function App() {
             if(lineTwo) {
               lineTwo.style.opacity = "1";
               lineTwo.style.height = "1px";
+            }
+  
+            if(header) {
+              header.style.position = "fixed";
             }
   
           } else {
@@ -165,6 +166,10 @@ function App() {
               lineTwo.style.opacity = "0.6";
               lineTwo.style.height = "2px";
             }
+  
+            if(header) {
+              header.style.position = "absolute";
+            }
 
           }
         }
@@ -183,7 +188,7 @@ function App() {
   return (
     <>
       <main>
-        <header>
+        <header id='header-main'>
           <div className="header-hamburger" onClick={handleNavLinks}>
             <span className="header-hamburger-item-1" ref={crossOne} id="header-line-1"></span>
             <span className="header-hamburger-item-2" ref={crossTwo} id="header-line-2"></span>
@@ -240,6 +245,70 @@ function App() {
 
         </div>
       </div>
+      <section className="info-dates-container">
+        <h2>
+        Friday—Sunday
+        10—12 August 2023
+        </h2>
+        <div></div>
+        <h1>
+        Brooklyn—NY
+        Clarkson House
+        </h1>
+        <p>
+        We were always taught that art creates problems and design solves them. Is that an overly limiting way to think of our practice? Are we closing ourselves off to interesting opportunities by not embracing the role of troublemakers?
+        </p>
+      </section>
+      <div className="info-banner-container">
+        <div className="info-section-one">
+          {
+            //first one height to 0
+          }
+          <div className="block-image-one" ref={showImagePartOne}></div>
+          {
+            //second one height to 0
+          }
+          <div className="block-image-two" ref={showImagePartTwo}></div>
+          <img src={carmine2} alt="camine image artists 1" />
+        </div>
+        <div className="info-section-two">
+          <h1>
+          Speakers include Mark Novo, Sofia Pazari, Sandra Cami, Kevin Matsuya, Chase Nevins, Ida Kester, Helen Gates and more.
+          </h1>
+        </div>
+      </div>
+      <div className="info-banner-container">
+        <div className="info-section-one">
+          {
+            //first one height to 0
+          }
+          <div className="block-image-one" ref={showImagePartOne}></div>
+          {
+            //second one height to 0
+          }
+          <div className="block-image-two" ref={showImagePartTwo}></div>
+          <img src={carmine3} alt="camine image artists 1" />
+        </div>
+        <div className="info-section-two">
+          <p>
+          Enjoy live demonstrations of papermaking, Japanese calligraphy, screenprinting and other craft traditions with applications to both art and design. 
+          </p>
+        </div>
+      </div>
+      <div className="action-buy-tickets">
+        <div className="buy-tickets">
+          <a href='#'>
+            Buy Tickets
+          </a>
+        </div>
+      </div>
+      <footer className="footer-container">
+        <ul className="socials">
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">Instagram</a></li>
+        </ul>
+      </footer>
     </>
   )
 }
